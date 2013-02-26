5242a78ea58d83dd7c87fb8111c7f73bba9f6d0a
Yoann <yoann_guerin@hotmail.com>
2013-02-26 15:06:15 +0100

    Avancement dans la mise en place de l'UI. C'est encore buggé, elle est affichée une fois sur cinq. Il faut que je trouve le moyen de créer l'UI une fois que la collection a bien été fetch/remplie sinon on crée l'ui alors que la collection est vide => rien ne s'affiche.

--- MaisonHote/src/main/resources/js/view/configChambre.js
+++ MaisonHote/src/main/resources/js/view/configChambre.js
@@ -9,22 +9,35 @@ window.EditChambreView = Backbone.View.extend({
 		$(this.el).append("<div id='chambre'></div>");
 		return this;
 	},
-	
-	chambres: function (){
-		var chambres = new Chambres();
-		chambres.localStorage = new Backbone.LocalStorage("chambres-backbone");
-		chambres.fetch();	
-		chambres.each(function(Chambre){
-			this.template = _.template(tpl.get('ChambreView'));
-			//ne marche pas pour append
-			$(this.el).append(this.template(Chambre.toJSON()));
-			$(this.el).append("<p> CA MARCHE </p>");
-			console.log($(this.el));
-			console.log("passage");
-			console.log(chambres.toJSON());
 
+	chambres: function (){
+
+		var self = this;
+
+		window.chambres = new Chambres();
+		window.chambres.localStorage = new Backbone.LocalStorage("chambres-backbone");
+		window.chambres.fetch({
+			success: function(model, response, options) {
+				chambres.each(function(Chambre){
+					this.template = _.template(tpl.get('ChambreView'));
+					$(self.el).append(this.template(Chambre.toJSON()));
+					console.log(chambres.toJSON());
+					console.log(model);
+					console.log(response);
+					console.log(options);
+				});
+			}
 		});
+
+
 		console.log("en dehors");
+
+		console.log($(this.el));
+		console.log(chambres.toJSON());
+
+		window.ls = new Backbone.LocalStorage("chambres-backbone");
+		window.arrayChambres = ls.findAll();
+
 		console.log(chambres.toJSON());
 		return this;
 	}
