79b34f5608800eebe2d4a2fd3905dd3da31b1910
Emilie Gachet <eilime_37@MacBook-de-Emilie-Gachet.local>
2013-02-26 17:33:38 +0100

    maj code js

--- MaisonHote/target/classes/js/view/header.js
+++ MaisonHote/target/classes/js/view/header.js
@@ -2,8 +2,10 @@ window.HeaderView = Backbone.View.extend({
 
 	events : {
 		"click #deconnexion" : "buttonClickHandler",
-		"click #fr"			 : "fr",
-		"click #en"			 : "en"
+		"click .dropdown-menu a" : "buttonLangage"
+		// "click #fr"			 : "fr",
+		// "click #en"			 : "en",
+		
 	},
 	
     initialize: function () {
@@ -19,7 +21,7 @@ window.HeaderView = Backbone.View.extend({
     	app.connexion();
     },
     
-    fr : function() {
+    buttonLanguage : function click(id) {
       var language = 'fr';
       $.ajax({
         url: 'language.xml',
@@ -31,19 +33,34 @@ window.HeaderView = Backbone.View.extend({
           });
         }
       });
-    },
+    }
     
-    en : function() {
-      var language = 'en';
-      $.ajax({
-        url: 'languages.xml',
-        success: function(xml) {
-          $(xml).find('translation').each(function(){
-            var id = $(this).attr('id');
-            var text = $(this).find(language).text();
-            $("#" + id).html(text);
-          });
-        }
-      });
-    } 
+    
+    // fr : function() {
+      // var language = 'fr';
+      // $.ajax({
+        // url: 'language.xml',
+        // success: function(xml) {
+          // $(xml).find('translation').each(function(){
+            // var id = $(this).attr('id');
+            // var text = $(this).find(language).text();
+            // $("#" + id).html(text);
+          // });
+        // }
+      // });
+    // },
+//     
+    // en : function() {
+      // var language = 'en';
+      // $.ajax({
+        // url: 'languages.xml',
+        // success: function(xml) {
+          // $(xml).find('translation').each(function(){
+            // var id = $(this).attr('id');
+            // var text = $(this).find(language).text();
+            // $("#" + id).html(text);
+          // });
+        // }
+      // });
+    // } 
 });
