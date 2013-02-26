a6a85ebc4efccce533439544d7c042d55e09447d
Yoann <yoann_guerin@hotmail.com>
2013-02-26 13:40:25 +0100

    Début de travail sur l'interface d'édition des chambres

--- MaisonHote/src/main/resources/js/view/configChambre.js
+++ MaisonHote/src/main/resources/js/view/configChambre.js
@@ -2,21 +2,30 @@ window.EditChambreView = Backbone.View.extend({
 
 	initialize: function () {
 		this.render();
-		var chambres = new Chambres();
-		chambres.localStorage = new Backbone.LocalStorage("chambres-backbone");
-		chambres.fetch({ 
-			success:function() {
-				chambres.each(function(Chambre){
-					this.template = _.template(tpl.get('ChambreView'));
-					$(this.el).append(this.template(Chambre.toJSON()));
-					console.log(Chambre);
-				});
-			}
-		});	
+		this.chambres();
 	},
 
 	render: function () {
 		$(this.el).append("<div id='chambre'></div>");
+		return this;
+	},
+	
+	chambres: function (){
+		var chambres = new Chambres();
+		chambres.localStorage = new Backbone.LocalStorage("chambres-backbone");
+		chambres.fetch();	
+		chambres.each(function(Chambre){
+			this.template = _.template(tpl.get('ChambreView'));
+			//ne marche pas pour append
+			$(this.el).append(this.template(Chambre.toJSON()));
+			$(this.el).append("<p> CA MARCHE </p>");
+			console.log($(this.el));
+			console.log("passage");
+			console.log(chambres.toJSON());
+
+		});
+		console.log("en dehors");
+		console.log(chambres.toJSON());
 		return this;
 	}
 });
--- MaisonHote/src/main/resources/js/view/connexion.js
+++ MaisonHote/src/main/resources/js/view/connexion.js
@@ -35,7 +35,7 @@ window.ConnexionView = Backbone.View.extend({
 				var showdata=localStorage.getItem('token-backbone-0');
 				console.log(showdata);
 
-				tpl.retrieveFile('house_config.json', function(reponse){
+				tpl.retrieveFile('house_config2.json', function(reponse){
 					if (reponse.items.length == 0) {
 						alert("PREMIERE UTILISATION");
 						app.firstConfigChambre();
