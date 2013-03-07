2cbba98fe4de28edd16ad2874179651bbaf32a31
Pierre33400 <pierre.sfy@outlook.com>
2013-03-07 09:45:49 +0100

    Format d'entrée du n° de tél. (fr) vérifié

--- MaisonHote/src/main/resources/js/view/resaDialog.js
+++ MaisonHote/src/main/resources/js/view/resaDialog.js
@@ -1,16 +1,13 @@
 function initResaDialog() {
-	$("input[name=nbPersons]").spinner({
-		spin: function(event, ui) {
-			if (ui.value > 10) {
-				$(this).spinner("value", 1);
-				return false;
-			} else if (ui.value < 1) {
-				$(this).spinner("value", 10);
-					return false;
-			}
-		}
-	}).val(1);
+	$("input[name=nbPersons]").spinner({min: 1});
 
+    jQuery.validator.addMethod("phone", function(value, element) {
+        if (value.length == 0) {
+        	return true;
+        }
+        return /^0\d \d\d \d\d \d\d \d\d$/i.test(value);
+    }, 'Expected phone number format: <i>0X XX XX XX XX</i>');
+ 
 	$('#resa-form').validate({
 		rules: {
 			lastName: {
@@ -20,7 +17,7 @@ function initResaDialog() {
 				required: true
 			},
 			phone: {
-				required: true
+				phone: true
 			},
 			email: {
 				email: true
