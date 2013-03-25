6c2ed683656f56fc2f82ffdf881d9e05ab5f2cc2
eldeftones <rog.florian@gmail.com>
2013-03-25 04:15:39 +0100

    Modification et suppression de client. Mise à jour de la liste et du message. modicifation de l'interface.

--- MaisonHote/src/main/resources/js/view/listCustomer.js
+++ MaisonHote/src/main/resources/js/view/listCustomer.js
@@ -1,75 +1,110 @@
 window.ListCustomerView = Backbone.View.extend({
 
+	idCustomer:null,
+	
 	events: {
 		//"click #btnSearchCustomer"   : "searchCustomer"
 		"click #btnAddCustomer": "addCustomer",
 		"click #btnClose" : "closeModal",
 		"click #btnSave" : "saveCustomer",
-		"click #selectCustomer" : "showCustomer"
+		"click #btnEditCustomer" : "showModalCustomer",
+		"click #selectCustomer" : "showCustomer",
+		"click #btnDeleteCustomer" : "deleteCustomer"
 	},
 
     initialize: function () {
     	customers = new Customers();
 		customers.localStorage = new Backbone.LocalStorage("customers-backbone");
 		customers.fetch();
+		idCustomer = '-1' ;
         this.render();
     },
 
-    render: function () {
-       //$(this.el).html(_.template(tpl.get('ListCustomerView')));
-              
-    	var list = "<div class='span5'><div class='hero-unit'><table class='table'><tr><td>" +
-    		        "<a id='btnAddCustomer' role='button' class='btn' data-toggle='modal'>+</a></td><td><select id='selectCustomer' size='10' >";
+    render: function () {       
+        var customersData = "<div class='span5'><div class='hero-unit'><table class='table'><tr><td>";
+        customersData += this.createList();
+        customersData += "</td><td><a id='btnAddCustomer' role='button' class='btn' data-toggle='modal'><img src='css/img/add_logo.png' width='30'  border='0'/></a><br/>"
+        	     + "<a id='btnEditCustomer' role='button' class='btn' data-toggle='modal'><img src='css/img/edit_logo.png' width='30'  border='0'/></a><br/>"
+        	     + "<a id='btnDeleteCustomer' role='button' class='btn'><img src='css/img/delete_logo.png' width='30'  border='0'/></a></td>"
+        	     + "</td></tr></table></div></div><div  class='span5'><div id='dataCustomer' class='hero-unit'>" + customers.length + " clients dans votre annuaire </div>";
+                
+        $(this.el).append(customersData);         
+        return this;
+    },
+    
+    reRender: function(){
+    	$('#selectCustomer').replaceWith(   this.createList() );    	
+    },
+    
+    createList: function(){
+    	var list = "<select id='selectCustomer' size='10' >";
         if(customers!=null)
         {
         	customers.each(function(Customer){
         		list += "<option value='" + Customer.get('id') + "' >" + Customer.get('name').toUpperCase() 
         															   + " " + Customer.get('firstname')
-        															   + " - " + Customer.get('address')  + "</option>";
+        															   + " (" + Customer.get('city')  + ")</option>";
         	});
         }
-        list += "</select></td></tr></table></div></div><div  class='span5'><div id='dataCustomer' class='hero-unit'>" + customers.length + " clients dans votre annuaire </div></div>";
-        
-        $(this.el).append(list);      
-       
-        return this;
-    },
+        list += "</select>";
+        return list ;
+    },   
     
-    addCustomer: function(){
-		customer = new Customer({'id': eval(customers.length+1)});
-		customers.add(customer);
-		
+    addCustomer: function(){		
 		this.template = _.template(tpl.get('DataCustomerView'));
-		$('#dataCustomer').before(this.template(customer.toJSON()));
+		$('#dataCustomer').before(this.template);
     	
       	// vide les infos du modal
 		$("#inputName").val( "" );
 		$("#inputFirstname").val( "" );
 		$("#inputPhone").val( "" );
-		$("#inputAdress").val( "" );
+		$("#inputAddress").val( "" );
 		$("#inputCp").val( "" );	
 		$("#inputCity").val( "" );	
 		$("#inputEmail").val( "" );	
-    	$('#myModal').modal('show');
+    	$('#myModal').modal('show');    	
     },
     
     closeModal: function(){
+    	this.reRender();
+    	idCustomer = '-1' ;
     	$('#myModal').modal('hide');
     },
     
-    saveCustomer: function(){
-		
+    saveCustomer: function(){		
 		 this.saveDataCustomer();		 
 		 this.closeModal();    	
     },
 	
+    getNewId : function(){
+    	var id = 0;
+    	if(customers!=null){
+	    	customers.each(function(Customer){
+	    		if (Customer.get('id') > id)
+	    			id = Customer.get('id') ;
+	    	});
+    	}
+    	id += 1;
+    	return id ;
+    },
+    
 	saveDataCustomer: function(){
-		console.log("clic enregister!");
-		success = true;
-
-		//enregistre tous les clients dans le cache
-		//customers.each(function(Customer){
-		console.log($('#inputName').val());
+		// If it'a new customer we have to create it
+		var msg2show ;
+		if ( idCustomer == '-1' ) {
+			var newId = this.getNewId();	    	
+			customer = new Customer({'id': newId});
+			customers.add(customer);
+			msg2show = 'Nouveau client ajouté avec succès.';
+		}
+		// else, we search the customer in the collection
+		else {					
+			customer = customers.get( idCustomer) ;
+			msg2show = 'Client modifié avec succès.'
+		}
+		
+		success = true;	
+		
 		window.name = $('#inputName').val();
 		window.firstname = $('#inputFirstname').val();
 		window.address = $('#inputAddress').val();
@@ -81,49 +116,59 @@ window.ListCustomerView = Backbone.View.extend({
 		customer.save({
 			'name':name, 'firstname':firstname, 'address':address,
 			'cp':cp, 'city':city, 'phone':phone, 'mail':mail
-		},
-		{
-
-			silent: true
+		},	{
+				silent: true
 		});
-
 		
-		//update le fichier sur le serveur
+		//update file on server
 		var obj = JSON.parse(localStorage.getItem("fichier-backbone-customers.json"));
-		console.log("dans collection customers"+customers);
-		updateFile(obj.idFichier, JSON.stringify(customers.toJSON()), function(reponse) {	
-			console.log(reponse);
-				});
+		updateFile(obj.idFichier, JSON.stringify(customers.toJSON()), function(){} );
+		document.getElementById('dataCustomer').innerHTML = msg2show ;
+		this.reRender();
 	},
 	
 	showCustomer : function (){
-		var idCustomer = document.getElementById('selectCustomer').value ;
+		idCustomer = document.getElementById('selectCustomer').value ;
 		var customer = JSON.parse( localStorage['customers-backbone-'+idCustomer] ) ;
 			
-		// charge les infos dans le div
+		// we load data in the div		
 		var infos = "<address><strong>"
 					+ customer.name.toUpperCase() + " "
 					+ customer.firstname + "</strong><br/>"
 					+ customer.address + "<br/>"
-					+ customer.cp + " " + customer.city + "<br/>"
+					+ customer.cp + " " + customer.city + "<br/><br/>"
 					+ customer.phone + "<br/>"					
 					+ "<a href='mailto:#'>" + customer.mail + "</a><br/></adresse>" ;		
 		document.getElementById("dataCustomer").innerHTML =  infos ;
-
-		console.log(customers);
-    	
+	},
+	
+	showModalCustomer : function (){
+		idCustomer = document.getElementById('selectCustomer').value ;
+		var customer = JSON.parse( localStorage['customers-backbone-'+idCustomer] ) ;
+		
 		this.template = _.template(tpl.get('DataCustomerView'));
 		$('#dataCustomer').before(this.template());
-		// charge les infos dans le modal et l'ouvre
+		
+		// we load data in the modal and open it
 		$("#inputName").val( customer.name );
 		$("#inputFirstname").val( customer.firstname );
 		$("#inputPhone").val( customer.phone );
-		$("#inputAdress").val( customer.address );
+		$("#inputAddress").val( customer.address );
 		$("#inputCp").val( customer.cp );	
 		$("#inputCity").val( customer.city );	
 		$("#inputEmail").val( customer.mail );			
-		$('#myModal').modal('show');
+		$('#myModal').modal('show');		
+	},
+	
+	deleteCustomer : function (){		 
+		customer = customers.get( document.getElementById('selectCustomer').value) ;		
+		localStorage.removeItem('customers-backbone-'+customer.get('id')) ;
+		customers.remove(customer);		
+		var obj = JSON.parse(localStorage.getItem("fichier-backbone-customers.json"));
+		updateFile(obj.idFichier, JSON.stringify(customers.toJSON()), function (){} );
+		document.getElementById('dataCustomer').innerHTML = "Client supprimé avec succès." ;
+		this.reRender();		
 	}
-
+	
 });
 
